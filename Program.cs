ea2d346a595ab562bdb9f0b71458957935721579
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-09 11:52:25 -0700

    peer review commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿//Zionn Showers
 //Mini Challenge # 2 - Add 2 Numbers
 //I made the user input 2 numbers, with the program turning those numbers into strings. Afterwards, I made the program convert those two strings into integers so that they can successfully add together without showing the wrong number or any other errors.
-//Peer Review:
-//Review:
+//Peer Review: Chris Estrada
+//Review: The code is clean and works and the flowchart is clear and easy to read. Good job!
 
 Console.Clear();
 
