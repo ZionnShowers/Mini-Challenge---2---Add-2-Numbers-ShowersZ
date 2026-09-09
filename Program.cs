976a8f44510bfbbc2390e3212ea5171e92bdcf0a
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-09 10:26:30 -0700

    update test

--- Program.cs
+++ Program.cs
@@ -7,3 +7,4 @@ string num2 = Console.ReadLine();
 int actualNum1 = Convert.ToInt32(num1);
 int actualNum2 = Convert.ToInt32(num2);
 Console.WriteLine($"Those numbers added together equal {actualNum1 + actualNum2}!");
+//test
