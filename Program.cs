0317b149d393033830f8c623c85e7327c499e416
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-09 10:24:37 -0700

    Mini Challenge # 2 - Add 2 Numbers

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Please input a number: ");
+string num1 = Console.ReadLine();
+Console.Write("Now input another number: ");
+string num2 = Console.ReadLine();
+int actualNum1 = Convert.ToInt32(num1);
+int actualNum2 = Convert.ToInt32(num2);
+Console.WriteLine($"Those numbers added together equal {actualNum1 + actualNum2}!");
